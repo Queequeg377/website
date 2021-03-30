2ab2078be902981036d6d9093358e51b15cf7dfd
Ben Siress <ben.siress@nccaky.org>
2021-03-30 16:34:42 -0500

    added delete buttons

--- projects/todo/main.js
+++ projects/todo/main.js
@@ -1,12 +1,20 @@
 document.getElementById("Bens-Button").addEventListener("click", function() {
-    document.getElementById("myUL").innerHTML = "<li>Hello</li>";
+    document.getElementById("myUL").innerHTML = "<li></li>";
   });
 
   document.getElementById("Add-Btn").addEventListener("click", function() {
     var input=document.getElementById("myInput")
     var ul=document.getElementById("myUL")
     var li=document.createElement("li")
-    li.innerHTML=input.value
+    li.innerHTML=`${input.value} <button class="closebutton">X</button>`
     input.value=""
     ul.appendChild(li)
   });
+
+  document.getElementById("myUL").addEventListener("click", function(e) {
+    console.log(e)
+    this.removeChild(e.target.parentElement)
+
+
+
+  })
